e444aaa8944fbf2da20706662c3359aa6a32a11c
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-07-19 19:39:42 -0600

    modify Editor file and App with styles on css file

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml'
@@ -6,22 +6,42 @@ import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import { Controlled as ControlledEditor } from 'react-codemirror2';
 
-export const Editor = ({ displayName }) => {
+const Editor = (props) => {
+  const {
+    language,
+    displayName,
+    value,
+    onChange
+  } = props
+  const [open, setOpen] = useState(true)
+
+  function handleChange(editor, data, value) {
+    onChange(value)
+  }
+
   return (
-    <div className='editor-container'>
+    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
       <div className="editor-title">
     {displayName}
-    <button>0/C</button>
+    <button
+    onClick={() => setOpen(prevOpen => !prevOpen)}
+    >0/C</button>
       </div>
     <ControlledEditor
-    onBeforeChange={handleChange}
-    value={value}
-    className='code-mirror-wrapper'
-    options={{
-      lineWrapping: true,
-      lint: true,
-      mode: language
-    }}
+      onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: 'material',
+          lineNumbers: true
+        }}
+      />
     </div>
   )
 }
+
+
+export default Editor
